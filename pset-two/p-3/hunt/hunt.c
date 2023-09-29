71ae45fed81222af68313ac0e0c12fbe9035c89e
ajaku <andyjaku13@gmail.com>
2023-09-29 15:00:37 -0400

    Test on wsl now

--- pset-two/p-3/hunt/hunt.c
+++ pset-two/p-3/hunt/hunt.c
@@ -7,10 +7,13 @@
 #include <sys/stat.h>
 #include <sys/stat.h>
 #include <linux/limits.h>
+#include <unistd.h>
 
-int compare_files(char *target, char *test) {
+#define BUFSIZE 4096
 
-}
+/*int compare_files(char *target, char *test) {
+
+}*/
 
 int r (char *dn, char *target)
 {
@@ -42,16 +45,35 @@ int r (char *dn, char *target)
             for (int i = latest; i < (latest + (int)strlen(de->d_name)); i++) {
                 cur_path[i] = de->d_name[i-latest];
             }
-            lstat(cur_path, &buf);
+            lstat(cur_path, &buf); // using lstat to print information about file
             if ((buf.st_mode & __S_IFMT) == __S_IFDIR) {
                 printf("Current Path: %s\nDirectory\n", cur_path);
                 r(cur_path, target);
             }
             if ((buf.st_mode & __S_IFMT) == __S_IFREG) {
                 printf("Current Path: %s\nRegular File\n", cur_path);
+                printf("File Size: %ld\n", buf.st_size);
+                printf("Inode Number: %ld\n", buf.st_ino);
             }
             if ((buf.st_mode & __S_IFMT) == __S_IFLNK) {
+                // need to run stat to determine information information about file that the symlink points to
                 printf("Current Path: %s\nSymlink\n", cur_path);
+                struct stat sym_buf;
+                stat(cur_path, &sym_buf);
+                if ((sym_buf.st_mode & __S_IFMT) == __S_IFREG) {
+                    printf("File Type: Regular File\n");
+                    printf("File Size: %ld\n", sym_buf.st_size);
+                    printf("Inode Number: %ld\n", sym_buf.st_ino);
+                    char link_path_buf[PATH_MAX];
+                    readlink(cur_path, link_path_buf, PATH_MAX);
+                    printf("Link to Path: %s\n", link_path_buf);
+                    char contents_of_link[BUFSIZE];
+                    int bits_read = read(link_path_buf, contents_of_link, BUFSIZE);
+                    printf("Bits read: %d\n", bits_read);
+                    printf("Contents of Link: %s\n", contents_of_link);
+                } else {
+                    printf("Not a regular file so ignoring\n");
+                }
             }
         }
     }
