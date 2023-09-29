aed8e77ced12e72f8a251c974bc3fb97988c2853
ajaku <andyjaku13@gmail.com>
2023-09-29 10:29:00 -0400

    Working pathname buffer

--- pset-two/p-3/hunt/hunt.c
+++ pset-two/p-3/hunt/hunt.c
@@ -6,8 +6,13 @@
 #include <sys/types.h> // to include
 #include <sys/stat.h>
 #include <sys/stat.h>
+#include <linux/limits.h>
 
-int r (char *dn)
+int compare_files(char *target, char *test) {
+
+}
+
+int r (char *dn, char *target)
 {
     DIR *dirp;
     struct dirent *de;
@@ -18,25 +23,35 @@ int r (char *dn)
         return -1;
     }
     errno = 0;
+
     while ((de=readdir(dirp)))
     {
-        char *cur_file = strdup(dn);
-        struct stat buf;
+        char cur_path[PATH_MAX] = "";
         if (!(strcmp(".", de->d_name)) || !(strcmp("..", de->d_name))) {
-            printf("Ignored\n");
+            continue;
         } else {
-            strcat(cur_file, "/");
-            strcat(cur_file, de->d_name);
-            printf("%s\n", cur_file);
-            lstat(cur_file, &buf);
+            struct stat buf;
+            int latest;
+            for (int i = 0; i < (int)strlen(dn); i++) {
+                cur_path[i] = dn[i];
+                latest = i;
+            }
+            latest++;
+            cur_path[latest] = '/';
+            latest++;
+            for (int i = latest; i < (latest + (int)strlen(de->d_name)); i++) {
+                cur_path[i] = de->d_name[i-latest];
+            }
+            lstat(cur_path, &buf);
             if ((buf.st_mode & __S_IFMT) == __S_IFDIR) {
-                printf("Directory\n");
+                printf("Current Path: %s\nDirectory\n", cur_path);
+                r(cur_path, target);
             }
             if ((buf.st_mode & __S_IFMT) == __S_IFREG) {
-                printf("Regular File\n");
+                printf("Current Path: %s\nRegular File\n", cur_path);
             }
             if ((buf.st_mode & __S_IFMT) == __S_IFLNK) {
-                printf("Symlink\n");
+                printf("Current Path: %s\nSymlink\n", cur_path);
             }
         }
     }
@@ -49,8 +64,8 @@ int r (char *dn)
 
 int main(int argc, char* argv[]) {
     if (argc != 3) { fprintf(stderr, "Incorrect number of arguments\n"); return -1;}
-    //char *target = argv[1];
+    char *target = argv[1];
     char *start = argv[2];
-    r(start);
+    r(start, target);
     return 0;
 }
