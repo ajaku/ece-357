2c3405b9162021b4e92c417b9e0214e6ed3d3c29
ajaku <andyjaku13@gmail.com>
2023-10-14 01:58:11 -0400

    Final verion

--- pset-three/p-3/myshell/myshell.c
+++ pset-three/p-3/myshell/myshell.c
@@ -12,12 +12,17 @@
 #include <sys/resource.h>
 
 #define MAX_READ 4096
+// Hoping we user doesn't have an SH filer larger than this
+#define MAX_SH_LENGTH 100000
 
-extern char **environ;
+// Stores current working directory
 char cwd[MAX_READ];
-int exit_status;
+// Store current exit status
+int exit_status = 0;
+// Stores shell for sh parsing
 char *shell = NULL;
 
+// Struct that contains parsed content from the input
 typedef struct parsed_s {
     char *redirections[5];
     //o_stdin, *oct_stdout, *oct_stderr, *oca_stdout, *oca_stderr;
@@ -70,23 +75,30 @@ int my_cd(char *dir) {
     return 0;
 }
 
-int parse_input(char *input, parsed_t *params) {
+int parse_input(char *input, parsed_t *params, int script) {
     char *token;
 
-    if (!(token = strtok(input, " \n"))) {
+    if ((token = strtok(input, " \n")) == NULL) {
+        // If reading from a script, can't recieve CTRL + D so it must be \n
+        if (script) {
+            return 1;
+        }
+
         // When null can either be because enter (new line) or CTRL + D. 
         // Need to distinguish between the two cases
         int c;
         if ((c = getchar()) == EOF) {
-            params->eof_recv = 1;
+            return 2;
         } else { fprintf(stderr, "Error tokenizing input %s - %s\n", input, strerror(errno)); }
+
         return 1;
     }
 
     params->args[params->args_idx] = my_dupe(token, 0);
     params->args_idx++;
+
+    // Want to skip lines with #
     if (!(strncmp(token, "#", 1))) {
-        printf("Ignore this line\n");
         return 1;
     }
 
@@ -191,11 +203,12 @@ int handle_redir(parsed_t *flags) {
     }
     return 0;
 }
+
 // Partly inspired by: https://www.csl.mtu.edu/cs4411.ck/www/NOTES/process/fork/exec.html
 void exec_cmd(char **argv, parsed_t *flags, int EXIT_FLAG) {
     pid_t pid;
     int   status;
-    pid_t child_pid;
+    pid_t child_pid; 
     struct rusage ru;
 
     if (EXIT_FLAG >= 0) {
@@ -213,30 +226,20 @@ void exec_cmd(char **argv, parsed_t *flags, int EXIT_FLAG) {
             break;
 
         case 0:
-            handle_redir(flags);
             child_pid = getpid();
 
-            printf("Shell %s\n", shell);
-
-            if(shell != NULL) {
-                printf("Shell exists\n");
+            if ((exit_status = handle_redir(flags)) != 1) {
+                child_pid = getpid();
+                
                 if (execvp(argv[0], argv) < 0) {
                     fprintf(stderr, "Exec failed with argument %s - %s\n", argv[0], strerror(errno));
-                    exit(1);
-                }
-            } else {
-                printf("wtf\n");
-                if (execvp(argv[0], argv) < 0) {
-                    fprintf(stderr, "Exec failed with argument %s - %s\n", argv[0], strerror(errno));
-                    exit(1);
-                }
-            }
-
+                    exit(127);
+                } 
+            } 
+            fprintf(stderr, "Exit status %d\n", exit_status);
             break;
 
         default:
-            handle_redir(flags);
-
             int rep_time = 0;
             if(wait3(&status, 0, &ru) == -1) {
                 fprintf(stderr, "wait3 failed\n");
@@ -247,10 +250,17 @@ void exec_cmd(char **argv, parsed_t *flags, int EXIT_FLAG) {
             if (WIFEXITED(status)) {
 
                 if (WEXITSTATUS(status) == 0) {
-                    fprintf(stderr, "Child process %d exited normally with return value of %d\n", child_pid, WEXITSTATUS(status));
-                } else { fprintf(stderr, "Child process %d failed with return value of %d\n", child_pid, WEXITSTATUS(status)); }
+                    fprintf(stderr, "Child process %d exited normally\n", child_pid);
+                } else { fprintf(stderr, "Child process %d failed with return value of %d\n", child_pid, WEXITSTATUS(status) + exit_status); }
 
-                exit_status = WEXITSTATUS(status); 
+                /* Attempting to capture correct exit value
+                 * Confused a bit about the difference between +127 and just standard return values?
+                 */
+                if (WEXITSTATUS(status == 0)) {
+                    exit_status += WEXITSTATUS(status) ; 
+                } else {
+                    exit_status = WEXITSTATUS(status) ; 
+                }
             } 
 
             if (WIFSIGNALED(status)) {
@@ -269,98 +279,128 @@ void exec_cmd(char **argv, parsed_t *flags, int EXIT_FLAG) {
     }
 }
 
+void check_builtin (parsed_t *my_params) {
+            my_params->args[my_params->args_idx+1] = NULL;
+            if(!(my_params->args[0])) {
+
+                if(my_params->eof_recv) {
+                    exec_cmd(NULL, NULL, 0);
+                }
+
+            } else if (!(strcmp(my_params->args[0], "pwd"))) {
+
+                my_pwd(cwd);
+                printf("%s\n", cwd);
+
+            } else if (!(strcmp(my_params->args[0], "cd"))) {
+
+                my_cd(my_params->args[1]);
+
+            } else if (!(strcmp(my_params->args[0], "exit"))) {
+
+                if((my_params->args[1] != NULL)) {
+                    // Inspired by: https://sentry.io/answers/char-to-int-in-c-and-cpp/
+                    char *output;
+                    long exit_val = strtol(my_params->args[1], &output, 0);
+                    exec_cmd(NULL, NULL, exit_val);
+
+                } else { exec_cmd(NULL, NULL, 1); }
+
+            } else { exec_cmd(my_params->args, my_params, -1); }
+}
+
+void free_all(parsed_t *my_params) {
+
+}
+
 int main(int argc, char* argv[]) {
+
     errno = 0;
+
     if (argc > 2) { fprintf(stderr, "Too many inputs\n"); return -1; }
+
     FILE *script = NULL;
-    int r_script;
+
     if (argc == 2) {
-        printf("Shell: %s\nScript: %s\n", argv[0], argv[1]);
+
         if(!(script = fopen(argv[1], "r"))) {
             fprintf(stderr, "Error opening input script %s - %s\n", argv[1], strerror(errno));
             return -1;
         }
-        r_script = 1;
-    }
-    
-   // int end_loop = 0;
-   int first = 1;
-   while(1) {
-        size_t input_size = MAX_READ;
-        ssize_t line;
-        char *input = (char *)malloc(input_size * sizeof(char)); // Remember to free!
-        if(input == NULL) {
-            fprintf(stderr, "Unable to allocate buffer\n");
-            exit(1);
-        }
-        if (!r_script) {
-            line = getline(&input, &input_size, stdin);
-        } else { line = getline(&input, &input_size, script); }
 
-        if (line < 0) {
-            fprintf(stderr, "Unable to read line, skipping\n");
-            goto skip;
-        }
-
-        parsed_t my_params = { .args_idx = 0 };
-
-        printf("r_script %d\t first %d\n", r_script, first);
-
-        if (r_script && first) {
-            // skip this iteration
-            // First line is assuming to be value of shell
-            printf("input %s\n");
-            if (input != NULL) {
-                shell = strdup(input);
-                printf("%s\n", shell);
-                first = 0;
-                goto skip;
-            } 
-        } 
-
-        printf("%s\n", input);
-        parse_input(input, &my_params);
-
-        /*
-         * Exit is "implemeneted" below
-         * Cases:
-         * -1: Don't exit
-         *  0: Exit with return value of 1
-         * >0: Exit with return value of parameter 
+        size_t len = 0;
+        char *input[MAX_SH_LENGTH];
+        int real_idx = 0;
+        // Inspired by: https://stackoverflow.com/questions/68996815/saving-getline-output-to-an-external-array
+        /* Pre-populate an array with input lines from shell script
+         * Motivation was that strangely, when using getline, if an error occured
+         * on a due to a command, it would be appended to the final line of getline,
+         * causing getline to go back to a previous point
          */
-
-
-        my_params.args[my_params.args_idx+1] = NULL;
-        if(!(my_params.args[0])) {
-
-            if(my_params.eof_recv) {
-                exec_cmd(NULL, NULL, 0);
+        for (int i = 0; i < MAX_SH_LENGTH; i++) {
+            len = 0;
+            if ((getline(&input[i], &len, script)) == -1) {
+                break;
             }
-            continue;
+            real_idx++;
+        }
+        // Would have been used to perform #!./myshell operation
+        shell = input[0];
+        /* When running a shell script we were instructed to place #!./myshell
+         * as the interpreter, yet I'm not entirely sure how to then use that
+         * to find the binaries properly, so that was not implemented.
+         * (Would've asked questions but unfortuantely this is already late, sorry)
+         */
+        for (int i = 1; i <= real_idx; i++) {
+            parsed_t my_params = { .args_idx = 0 };
 
-        } else if (!(strcmp(my_params.args[0], "pwd"))) {
+            int r_parse = parse_input(input[i], &my_params, 1);
 
-            my_pwd(cwd);
-            printf("%s\n", cwd);
 
-        } else if (!(strcmp(my_params.args[0], "cd"))) {
+            // r_parse = 0 => CTRL + D has been received so exit
+            // r_parse = 1 => Comment or empty so skip
+            if (r_parse == 1) { goto skip_script; }
+            if (r_parse == 2) { my_params.eof_recv = 1; }
 
-            my_cd(my_params.args[1]);
+            check_builtin(&my_params);
 
-        } else if (!(strcmp(my_params.args[0], "exit"))) {
+            skip_script:
+                continue;
 
-            if((my_params.args[1] != NULL)) {
-                // Inspired by: https://sentry.io/answers/char-to-int-in-c-and-cpp/
-                char *output;
-                long exit_val = strtol(my_params.args[1], &output, 0);
-                exec_cmd(NULL, NULL, exit_val);
-            } else {
-                exec_cmd(NULL, NULL, 1);
-            }
+            i++;
 
-        } else { exec_cmd(my_params.args, &my_params, -1); }
+            for (int j = 0; j < my_params.args_idx; j++ ) { free(my_params.args[j]); }
+            for (int k = 0; k < 5 ; k++ ) { free(my_params.redirections[k]); }
+        }
+        // Would be done with script at this point
+        for (int i = 0; i < real_idx; i++) {
+            free(input[i]);
+        }
+    } else {
+        while(1) {
+            size_t len = 0;
+            ssize_t read;
+            char *input = NULL; // Remember to free!
+            read = getline(&input, &len, stdin);
 
-        skip:
-            continue;
-   }
+            parsed_t my_params = { .args_idx = 0 };
+
+            int r_parse = parse_input(input, &my_params, 0);
+
+            // r_parse = 0 => CTRL + D has been received so exit
+            // r_parse = 1 => Comment or empty so skip
+            if (r_parse == 1) { goto skip_stdin; }
+            if (r_parse == 2) { my_params.eof_recv = 1; }
+
+            check_builtin(&my_params); 
+
+            skip_stdin:
+                continue;
+
+            for (int i = 0; i < my_params.args_idx; i++ ) { free(my_params.args[i]); }
+            for (int k = 0; k < 5 ; k++ ) { free(my_params.redirections[k]); }
+            free(input);
+        }
+    }
+    return exit_status;
 }
