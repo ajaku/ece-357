b57c05307b0c9aa3a396b4182efdbd3a2cc4a064
ajaku <andyjaku13@gmail.com>
2023-11-03 10:09:19 -0400

    Add signal handler

--- pset-four/p-3/launcher.c
+++ pset-four/p-3/launcher.c
@@ -15,7 +15,10 @@ int main(int argc, char* argv[]) {
 
     if (argc > 2)  { fprintf(stderr, "Too many input arguments\n"); return 1; }
 
+    int pid[3];
     int pfds[2][2];
+
+    int status[3];
 
     /*
      * pfds[0][0] - Wordsearch Reader   fd[0][1] - Wordgen Writer 
@@ -34,9 +37,9 @@ int main(int argc, char* argv[]) {
     // wordsearch -> reads from wordgen, writes to pager
     // pager -> reads from wordsearch, writes to stdout
 
-    int pid1 = fork();
-    if (pid1 == -1) { perror("Fork Failed"); return 1; }
-    if (pid1 ==  0) {
+    pid[0] = fork();
+    if (pid[0] == -1) { perror("Fork Failed"); return 1; }
+    if (pid[0] ==  0) {
 
         close(pfds[0][0]);
         //close(pfds[0][1]);
@@ -52,11 +55,13 @@ int main(int argc, char* argv[]) {
         } else {
             execl("./wordgen", "./wordgen", argv[1], NULL);
         }
+        fprintf(stderr, "\n\nWORDGEN EXITED\n");
+        exit(0);
     }
 
-    int pid2 = fork();
-    if (pid2 == -1) { perror("Fork Failed"); return 1; }
-    if (pid2 ==  0) {
+    pid[1] = fork();
+    if (pid[1] == -1) { perror("Fork Failed"); return 1; }
+    if (pid[1] ==  0) {
 
         //close(pfds[0][0]);
         close(pfds[0][1]);
@@ -68,12 +73,14 @@ int main(int argc, char* argv[]) {
         dup2(pfds[1][1], STDOUT_FILENO);
         close(pfds[1][1]);
         execl("./wordsearch", "./wordsearch", "shorter_words.txt", NULL);
+        fprintf(stderr, "\n\nWORDSEARCH EXITED\n");
+        exit(0);
 
     }
 
-    int pid3 = fork();
-    if (pid3 == -1) { perror("Fork Failed"); return 1; }
-    if (pid3 ==  0) {
+    pid[2] = fork();
+    if (pid[2] == -1) { perror("Fork Failed"); return 1; }
+    if (pid[2] ==  0) {
 
         close(pfds[0][0]);
         close(pfds[0][1]);
@@ -83,16 +90,44 @@ int main(int argc, char* argv[]) {
         dup2(pfds[1][0], STDIN_FILENO);
         close(pfds[1][0]);
         execl("./pager", "./pager", NULL);
+        exit(0);
     }
 
     close(pfds[0][0]);
     close(pfds[0][1]);
     close(pfds[1][0]);
     close(pfds[1][1]);
+    
+    for (int i = 0; i < 3; i++) {
+        waitpid(pid[i], &status[i], 0);
+        if (WIFEXITED(status[i])) {
+            fprintf(stderr, "Child %d exited with %d\n", pid[i], WEXITSTATUS(status[i]));
+        }
+        if (WIFSIGNALED(status[i])) {
+            fprintf(stderr, "Child %d exited with %d\n", pid[i], WTERMSIG(status[i]));
+        }
+    }
 
-    waitpid(pid1, NULL, 0);
-    waitpid(pid2, NULL, 0);
-    waitpid(pid3, NULL, 0);
+    /*
+    waitpid(pid[0], &status[0], 0);
+    if (WIFEXITED(status[0])) {
+        fprintf(stderr, "Child (wordgen) %d exited with %d\n", pid[0], WEXITSTATUS(status[0]));
+    }
+    if (WIFSIGNALED(status[0])) {
+        fprintf(stderr, "Child (wordgen) %d exited with %d\n", pid[0], WTERMSIG(status[0]));
+    }
+    waitpid(pid[1], &status[1], 0);
+    if (WIFEXITED(status[1])) {
+        fprintf(stderr, "Child (wordsearch) %d exited with %d\n", pid[1], WEXITSTATUS(status[1]));
+    }
+    if (WIFSIGNALED(status[1])) {
+        fprintf(stderr, "Child (wordsearch) %d exited with %d\n", pid[1], WTERMSIG(status[1]));
+    }
+    waitpid(pid[2], &status[2], 0);
+    if (WIFEXITED(status[2])) {
+        fprintf(stderr, "Child (pager) %d exited with %d\n", pid[2], WEXITSTATUS(status[2]));
+    }
+    */
 
     return 0;
 }
